3e7c61686dcc1a4588cade2803ae2b3764d77d29
Oakley Brunt <oakley.brunt@metoffice.gov.uk>
2025-03-10 10:13:31 +0000

    #71 Update gitignore

--- docs/source/demos/fortran/empty.F90
+++
@@ -1,9 +0,0 @@
-! (C) Crown Copyright 2023, Met Office. All rights reserved.
-!
-! This file is part of PSyACC and is released under the BSD 3-Clause license.
-! See LICENSE in the root of the repository for full licensing details.
-
-PROGRAM intro
-  IMPLICIT NONE
-  PRINT *, "Hello, Fortran World!"
-END PROGRAM intro
